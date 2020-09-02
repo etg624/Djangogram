5e75864010fb56787ee6abd49b1c9c5fb13139f3
Evan Guirino <etg624@gmail.com>
2020-09-01 18:04:32 -0700

    added login signup links to header and fixed page error if no profile pic is set by the user

--- users/models.py
+++ users/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    profile_pic = models.ImageField(null=True, blank=True, upload_to="uploads/")
+    profile_pic = models.ImageField(null=True, blank=True, upload_to="uploads/", default="assets/")
     def __str__(self):
         return self.username
